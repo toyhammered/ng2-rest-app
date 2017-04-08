28555922815664c79bed696b31ef494f59af73ac
toyhammered <daniel@rassiner.com>
2017-04-08 13:05:00 -0700

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ItemsComponent } from './items/items.component';
+import { FeaturedItemComponent } from './items/featured-item/featured-item.component';
+
+const routes: Routes = [
+  {path: '',      redirectTo: '/items', pathMatch: 'full' },
+  {path: 'items', component: ItemsComponent, children: [
+    {path: 'featured/:id', component: FeaturedItemComponent}
+  ]},
+  {path: '**',     redirectTo: '/items', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: []
+})
+export class Ng2RestAppRoutingModule { }
