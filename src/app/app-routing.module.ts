3374d67ee9820106f4d2e241715dec8af397a115
toyhammered <daniel@rassiner.com>
2017-04-12 15:38:56 -0700

    add widget component and link to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ItemsComponent } from './items/items.component';
 import { FeaturedItemComponent } from './items/featured-item/featured-item.component';
+import { WidgetsComponent } from './widgets/widgets.component';
 
 const routes: Routes = [
-  {path: '',      redirectTo: '/items', pathMatch: 'full' },
+  {path: '',      redirectTo: '/widgets', pathMatch: 'full' },
   {path: 'items', component: ItemsComponent, children: [
     {path: 'featured/:id', component: FeaturedItemComponent}
   ]},
-  {path: '**',     redirectTo: '/items', pathMatch: 'full'}
+  {path: 'widgets', component: WidgetsComponent },
+  {path: '**',     redirectTo: '/widgets', pathMatch: 'full'}
 ];
 
 @NgModule({
